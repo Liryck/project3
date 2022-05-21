8932aa4ed97b5b6fc1c808a5ca77a62b0d306870
Liryck <liryckbragin@gmail.com>
2022-05-21 18:54:51 +0300

    add validate form and mask number

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+//Carousel
+
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
@@ -15,6 +17,7 @@ $(document).ready(function(){
         ]
     });
 
+        // Tabs
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
@@ -33,4 +36,57 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    //Modal
+
+    $('[data-modal=consultation]').on('click', function(){
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click', function(){
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
+    //Validate form
+
+    function valideForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Веедите как минимум {0} символа!")
+                },
+                phone: "Пожалуйста, введите свой телефон",
+                email: {
+                required: "Пожалуйста, введите свою почту",
+                email: "Неправильный формат почты"
+                }
+            }
+        });
+    }
+
+    valideForms('#consultation-form');
+    valideForms('#consultation form');
+    valideForms('#order form');
+
+    $('input[name=phone]').mask("+38 (999) 999-9999");
+
   });
